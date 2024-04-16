6f2844342c7baa013de011a58a1e884df803b6d9
ahmed ehab <ahmedmedo012556@gmail.com>
2024-04-15 20:20:58 -0700

    change at task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#!/usr/bin/pup
-# install puppet-lint -v 2.5.0
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+#This code will install the package puppet-line
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
 }
